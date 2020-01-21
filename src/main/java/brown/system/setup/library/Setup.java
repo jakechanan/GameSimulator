31226481ded76f51a93a3ce2f0dc9ae0624727ea
jakechanan <jake_chanan@brown.edu>
2020-01-21 16:06:41 -0500

    remove unused imports and unused agent.

--- src/main/java/brown/system/setup/library/Setup.java
+++ src/main/java/brown/system/setup/library/Setup.java
@@ -1,32 +1,15 @@
 package brown.system.setup.library;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
 import java.net.URISyntaxException;
-import java.net.URL;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Stream;
 
 import com.esotericsoftware.kryo.Kryo;
 
-import brown.logging.library.ErrorLogging;
-import brown.simulations.RpsSimulation;
 import brown.system.setup.ISetup;
 
 public final class Setup implements ISetup {
