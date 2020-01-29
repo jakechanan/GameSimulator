d7718aa351661342245f1ed9b3790214ddd0000a
jakechanan <jake_chanan@brown.edu>
2020-01-29 12:29:52 -0500

    BoS II Utility and AbsBoSIIAgent

--- src/main/java/brown/auction/rules/utility/BoSIIUtility.java
+++ src/main/java/brown/auction/rules/utility/BoSIIUtility.java
@@ -25,6 +25,7 @@ public class BoSIIUtility extends AbsRule implements IUtilityRule {
         Map<Integer, Double> utils = new HashMap<>();
         
         int STUBBORN = 0, COMPROMISE = 1;
+        int GOOD_MOOD = 0, BAD_MOOD = 1;
 
         IActionMessage a1Message = messages.get(0);
         IActionMessage a2Message = messages.get(1);
@@ -41,15 +42,60 @@ public class BoSIIUtility extends AbsRule implements IUtilityRule {
         double a1Util;
         double a2Util;
 
-        if (a1Action == a2Action) {
-            a1Util = 0;
-            a2Util = 0;
-        } else if (a1Action == STUBBORN) {
-            a1Util = 7;
-            a2Util = 3;
+        if (a1Mood == null) {
+        	if (a2Mood.equals(GOOD_MOOD)) {
+        		if (a1Action == a2Action) {
+        			a1Util = 0.0;
+        			a2Util = 0.0;
+        		} else if (a1Action == STUBBORN) {
+        			a1Util = 7.0;
+        			a2Util = 3.0;
+        		} else {
+        			a1Util = 3.0;
+        			a2Util = 7.0;
+        		}
+        	} else {
+        		if (a1Action == STUBBORN && a2Action == STUBBORN) {
+        			a1Util = 0.0;
+        			a2Util = 7.0;
+        		} else if (a1Action == COMPROMISE && a2Action == COMPROMISE) {
+        			a1Util = 0.0;
+        			a2Util = 3.0;
+        		} else if (a1Action == STUBBORN && a2Action == COMPROMISE) {
+        			a1Util = 7.0;
+        			a2Util = 0.0;
+        		} else {
+        			a1Util = 3.0;
+        			a2Util = 0.0;
+        		}
+        	}
         } else {
-            a1Util = 3;
-            a2Util = 7;
+        	if (a1Mood.equals(GOOD_MOOD)) {
+        		if (a1Action == a2Action) {
+        			a2Util = 0.0;
+        			a1Util = 0.0;
+        		} else if (a2Action == STUBBORN) {
+        			a2Util = 7.0;
+        			a1Util = 3.0;
+        		} else {
+        			a2Util = 3.0;
+        			a1Util = 7.0;
+        		}
+        	} else {
+        		if (a1Action == STUBBORN && a2Action == STUBBORN) {
+        			a2Util = 0.0;
+        			a1Util = 7.0;
+        		} else if (a1Action == COMPROMISE && a2Action == COMPROMISE) {
+        			a2Util = 0.0;
+        			a1Util = 3.0;
+        		} else if (a2Action == STUBBORN && a1Action == COMPROMISE) {
+        			a2Util = 7.0;
+        			a1Util = 0.0;
+        		} else {
+        			a2Util = 3.0;
+        			a1Util = 0.0;
+        		}
+        	}
         }
         
         utils.put(agent1, a1Util);
--- src/main/java/brown/user/agent/library/AbsBoSIIAgent.java
+++ src/main/java/brown/user/agent/library/AbsBoSIIAgent.java
@@ -10,24 +10,31 @@ import brown.communication.messages.ISimulationReportMessage;
 import brown.communication.messages.ITypeMessage;
 import brown.communication.messages.library.ActionMessage;
 import brown.logging.library.UserLogging;
+import brown.platform.accounting.IAccountUpdate;
 import brown.system.setup.ISetup;
 import brown.user.agent.IAgent;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import brown.auction.type.valuation.library.BoSIIType;
 
 public abstract class AbsBoSIIAgent extends AbsAgent implements IAgent {
-  
   private Integer auctionID;
   
   private Integer mood;
+  private List<Move> moveHistory;
+  
+  private static final Integer GOOD_MOOD = 0, BAD_MOOD = 1;
+  private static final Integer STUBBORN = 0, COMPROMISE = 1;
   
   public AbsBoSIIAgent(String host, int port, ISetup gameSetup,
       String name) {
     super(host, port, gameSetup, name);
     this.mood = null;
     this.auctionID = null;
+    this.moveHistory = new LinkedList<>();
   }
 
   @Override
@@ -43,6 +50,38 @@ public abstract class AbsBoSIIAgent extends AbsAgent implements IAgent {
 	  return this.mood;
   }
   
+  public List<Move> getMoveHistory() {
+	  return new ArrayList<>(this.moveHistory);
+  }
+  
+  public Double opponentGoodMoodProbability() {
+	  return (isRowPlayer()) ? (2.0 / 3) : null;
+  }
+  
+  public Double rowPlayerRewardFrom(Integer rowPlayerMove, Integer columnPlayerMove) {
+	  if (rowPlayerMove.equals(columnPlayerMove)) {
+		  return 0.0;
+	  } else {
+		  return (rowPlayerMove.equals(STUBBORN)) ? 7.0 : 3.0;
+	  }
+  }
+  
+  public Double columnPlayerRewardFrom(Integer rowPlayerMove, Integer columnPlayerMove, Integer columnPlayerMood) {
+	  if (columnPlayerMood.equals(GOOD_MOOD)) {
+		  if (rowPlayerMove.equals(columnPlayerMove)) {
+			  return 0.0;
+		  } else {
+			  return (columnPlayerMove.equals(STUBBORN)) ? 7.0 : 3.0;
+		  }
+	  } else {
+		  if (!rowPlayerMove.equals(columnPlayerMove)) {
+			  return 0.0;
+		  } else {
+			  return (columnPlayerMove.equals(STUBBORN)) ? 7.0 : 3.0;
+		  }
+	  }
+  }
+ 
   public abstract Integer nextMove();
 
   @Override
@@ -72,15 +111,60 @@ public abstract class AbsBoSIIAgent extends AbsAgent implements IAgent {
   @Override
   public void
       onSimulationReportMessage(ISimulationReportMessage simulationMessage) {
-	  List<IActionMessage> history = simulationMessage.getMarketResults().get(this.auctionID).getTradeHistory().get(0);
-      for (IActionMessage msg : history) {
-      	if (!msg.getAgentID().equals(this.publicID)) {
-      		Integer bid = ((GameAction)msg.getBid()).getAction();
-      		Integer mood = ((BoSIIAction)msg.getBid()).getMood();
-      		//this.opponentLastMove = bid;
-      		break;
-      	}
-      }
+	  this.moveHistory.add(new Move(simulationMessage, this));
+  }
+  
+  protected static class Move {
+	  private Integer myMove;
+	  private Integer opponentMove;
+	  private Integer columnPlayerMood;
+	  private Double myReward;
+	  private Double opponentReward;
+	  
+	  public Move(ISimulationReportMessage msg, AbsBoSIIAgent agent) {
+		  List<IActionMessage> history = msg.getMarketResults().get(agent.auctionID).getTradeHistory().get(0);
+	      for (IActionMessage act : history) {
+	    	Integer bid = ((GameAction)act.getBid()).getAction();
+	      	if (msg.getAgentID().equals(agent.publicID)) {
+	      		this.myMove = bid;
+	      	} else {
+	      		this.opponentMove = bid;
+	      	}
+
+      		Integer mood = ((BoSIIAction)act.getBid()).getMood();
+	      	if (mood != null) {
+	      		this.columnPlayerMood = mood;
+	      	}
+	      }
+	      
+	      for (IAccountUpdate upd : msg.getMarketResults().get(agent.auctionID).getUtilities()) {
+	        	if (upd.getTo().equals(agent.publicID)) {
+	        		this.myReward = upd.getCost();
+	        	} else {
+	        		this.opponentReward = upd.getCost();
+	        	}
+	        }
+	  }
+
+	public Integer getMyMove() {
+		return myMove;
+	}
+
+	public Integer getOpponentMove() {
+		return opponentMove;
+	}
+
+	public Integer getColumnPlayerMood() {
+		return columnPlayerMood;
+	}
+
+	public Double getMyReward() {
+		return myReward;
+	}
+
+	public Double getOpponentReward() {
+		return opponentReward;
+	}
   }
 
 }
