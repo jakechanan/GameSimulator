4bb5acaf90200ab3abc487b63a91773c6c4e41da
jakechanan <jake_chanan@brown.edu>
2021-02-07 09:47:01 -0600

    stag hunt opponent

--- src/main/java/brown/user/agent/library/offline/MysteryStagHuntOpponent.java
+++ src/main/java/brown/user/agent/library/offline/MysteryStagHuntOpponent.java
@@ -6,39 +6,50 @@ import brown.user.agent.library.offline.AbsOfflineLearningAgent;
 
 public class MysteryStagHuntOpponent extends AbsOfflineLearningAgent {
 	private static final int STAG = 0, HARE = 1;
+	private int round;
 
 	public MysteryStagHuntOpponent() {
 		super();
+		this.round = 0;
 	}
 
 	@Override
 	public int nextMove() {
+		if (this.round == 0) {
+			return HARE;
+		}
+		
+		if (this.round % 6 == 0) {
+			return STAG;
+		}
+		
+		if (this.round % 6 == 2) {
+			return HARE;
+		}
+		
 		List<Integer> opponentMoves = this.getOpponentActions(0);
-		if (opponentMoves.size() < 5 || opponentMoves.size() % 4 == 0) {
+		
+		if (opponentMoves.size() < 10) {
+			return (int)(Math.random() * 2);
+		}
+		
+		int j = 0;
+		for (int i = 1; i <= 10; i++) {
+			if (opponentMoves.get(opponentMoves.size() - i).intValue() == HARE) {
+				j++;
+			}
+		}
+		if (j % 2 == 0) {
 			return HARE;
 		} else {
-			if (opponentMoves.get(opponentMoves.size() - 1) == HARE) {
-				return HARE;
-			}
-			
-			int j = 0;
-			for (int i = 1; i <= 5; i++) {
-				if (opponentMoves.get(opponentMoves.size() - i).intValue() == HARE) {
-					j++;
-				}
-			}
-			if (j > 2) {
-				return HARE;
-			} else {
-				return STAG;
-			}
+			return STAG;
 		}
 	}
 
 	@Override
 	public void afterRound() {
 		// TODO Auto-generated method stub
-
+		this.round++;
 	}
 
 }
